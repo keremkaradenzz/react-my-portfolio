71315623035ef164d1e6176f1c5be668c763e680
kerem <keremkaradenzz@gmail.com>
2022-04-09 21:56:36 +0300

    development my-portfolio site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { useTransition, animated, config } from 'react-spring'
+import { TransitionMotion, spring } from 'react-motion';
+import './App.css';
+const leavingSpringConfig = { stiffness: 60, damping: 15 };
+
+function TransitionArray() {
+  const [items, setItems] = useState([
+    { id: 1, text: "Hi I'm Kerem Karadeniz " },
+  ]);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      setTimeout(() => {
+        setItems([
+          { id: 1, text: "Hi I'm Kerem Karadeniz " },
+        ]
+        )
+      }, 2000)
+    }
+  }, [items])
+
+  const transition = useTransition(items, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    delay: 200,
+    config: config.molasses,
+    onRest: () => setItems([]),
+  })
+
+  const fragment = transition((style, item) => {
+    return <animated.div style={
+      style
+    }><h1>{item.text}</h1></animated.div>;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{fragment}</div>
+    </>
+  );
+}
+function App() {
+  const [state, setState] = useState({ mouse: [], now: 't' + 0 });
+  const handleMouseMove = ({ pageX, pageY }) => {
+    // Make sure the state is queued and not batched.
+    setState(() => {
+      return {
+        mouse: [pageX - 25, pageY - 25],
+        now: 't' + Date.now(),
+      };
+    });
+  };
+
+
+  const handleTouchMove = (e) => {
+    e.preventDefault();
+    handleMouseMove(e.touches[0]);
+  };
+
+  const willLeave = styleCell => {
+    return {
+      ...styleCell.style,
+      opacity: spring(0, leavingSpringConfig),
+      scale: spring(2, leavingSpringConfig),
+    };
+  };
+
+
+  const { mouse: [mouseX, mouseY], now } = state;
+  const styles = mouseX == null ? [] : [{
+    key: now,
+    style: {
+      opacity: spring(1),
+      scale: spring(0),
+      x: spring(mouseX),
+      y: spring(mouseY),
+    }
+  }];
+
+  return (
+      <TransitionMotion willLeave={willLeave} styles={styles}>
+        {circles =>
+          <div
+            onMouseMove={handleMouseMove}
+            onTouchMove={handleTouchMove}
+            className="demo7">
+            {circles.map(({ key, style: { opacity, scale, x, y } }) =>
+              <div
+                key={key}
+                className="demo7-ball"
+                style={{
+                  opacity: opacity,
+                  scale: scale,
+                  transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
+                  WebkitTransform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
+                }} />
+            )}
+           <main className="main">
+            <div className='title'>
+                <TransitionArray />
+            </div>
+           
+            </main>
+          </div>
+        
+          }
+      </TransitionMotion>
   );
 }
 
